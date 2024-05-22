da8fb53f0338256e9fd51bf1fc7eb89e531bfef9
JohnKaranja <jonniekaras@gmail.com>
2024-05-22 20:31:44 +0300

    Passed all tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    x = []
+    for i in num_list:
+        if i %2 == 0:
+            x.append(i)
+    return x
 
 def make_exclamation(sentence_list):
-    pass
+    sentence = []
+    for i in sentence_list:
+        sentence.append(i+"!")
+    return sentence
+
